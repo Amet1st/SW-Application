3b947a4c953b1d73ca3fa5b8078a779281934fef
Nikolay Naidenov <mikalay.naydzenau@gmail.com>
2022-07-28 12:59:30 +0300

    add: implemented search functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 const ITEMS_ON_PAGE = 10;
 const BASE_URL = "https://swapi.dev/api/people/";
+let example = "https://swapi.dev/api/people/?search=r&page=2";
+
 
 const pagination = document.getElementById('pagination');
 const itemsList = document.getElementById('items');
+const searchForm = document.getElementById('search');
 
 let expandButtons;
 let hideButtons;
 let active;
+let search;
 
 document.addEventListener('DOMContentLoaded', showInitialItems);
 
 pagination.addEventListener('click', showItems);
+
+searchForm.addEventListener('submit', getSearchedData);
 
 async function getData(url) {
     let response = await fetch(url);
@@ -19,18 +25,30 @@ async function getData(url) {
 
 }
 
-function createPaginationItems(num) {
+function createPaginationItems(num, search) {
+
+    pagination.innerHTML = "";
 
     for (let i = 1; i <= num; i++) {
-        let itemHTML = `
+
+        let itemHTML;
+
+        if (search) {
+            itemHTML = `
+            <li class="items__list-item"><a class="items__link" href="#?search=${search}&page=${i}">${i}</a></li>
+            `;
+        } else {
+            itemHTML = `
             <li class="items__list-item"><a class="items__link" href="#?page=${i}">${i}</a></li>
-        `;
+            `;
+        }
+        
 
         pagination.insertAdjacentHTML('beforeend', itemHTML);
     }
 }
 
-async function showItems(event) {
+async function showItems(event, search) {
     if (event.target.classList.contains('items__link')) {
 
         if (active) {
@@ -40,7 +58,13 @@ async function showItems(event) {
         active = event.target.closest('li');
         active.classList.add('active');
 
-        let url = BASE_URL + `?page=${event.target.textContent}`;
+        let url;
+
+        if (search) {
+            url = BASE_URL + `?search=${search}&page=${event.target.textContent}`;
+        } else {
+            url = BASE_URL + `?page=${event.target.textContent}`;
+        }
 
         const data = await getData(url);
 
@@ -67,9 +91,9 @@ function createItemHTML(item) {
 
     let html = `<li>
                     <p>Name: ${name}</p>
-                    <p>Birth Year: ${birth_year}</p>
-                    <p>Gender: ${gender}</p>
                     <button class="expand-toggle toggle">Expand</button>
+                    <p class="second-data hide">Birth Year: ${birth_year}</p>
+                    <p class="second-data hide">Gender: ${gender}</p>
                     <p class="second-data hide">Height: ${height}</p>
                     <p class="second-data hide">Mass: ${mass}</p>
                     <p class="second-data hide">Hair Color: ${hair_color}</p>
@@ -83,11 +107,18 @@ function createItemHTML(item) {
 }
 
 
-async function showInitialItems(event) {
+async function showInitialItems(search) {
+
+    itemsList.innerHTML = "";
+
     let location = document.location.href;
 
     let url;
     let pageID = "";
+
+    if (search) {
+
+    }
 
     if (location.indexOf('page') != -1) {
         pageID = location.substr(location.indexOf('page') + 5, 1);
@@ -154,4 +185,84 @@ function addButtonEvents() {
             li.querySelector('.expand-toggle').classList.remove('hide');
         });
     });
+}
+
+async function getSearchedData(event) {
+
+    event.preventDefault();
+
+    const input = document.querySelector('input');
+
+    if (input.value) {
+
+        itemsList.innerHTML = "";
+
+        search = input.value;
+
+        const url = BASE_URL + `?search=${search}`;
+        const data = await getData(url);
+
+         if (!data.results.length) {
+             itemsList.insertAdjacentHTML('beforeend', `<h2 style="color: white; font-size: 40px;">No results</h2>`);
+        }
+
+        let pageItems = Math.ceil(data.count / ITEMS_ON_PAGE);
+        createPaginationItems(pageItems, input.value);
+        
+        active = pagination.firstElementChild;
+        active.classList.add('active');
+
+        const items = data.results;
+
+        items.forEach(item => {
+            let itemHTML = createItemHTML(item);
+
+            itemsList.insertAdjacentHTML('beforeend', itemHTML);
+        });
+
+        pagination.removeEventListener('click', showItems);
+        pagination.addEventListener('click', event => {
+
+            if (event.target.classList.contains('items__link')) {
+                showSerchedItems(event);
+            }
+        });
+
+        expandButtons = document.querySelectorAll('.expand-toggle');
+        hideButtons = document.querySelectorAll('.hide-toggle');
+
+        addButtonEvents();
+
+        searchForm.reset();
+    }
+}
+
+
+async function showSerchedItems(event) {
+
+    if (active) {
+        active.classList.remove('active');
+    }
+
+    active = event.target.closest('li');
+    active.classList.add('active');
+
+    let url = BASE_URL + `?search=${search}&page=${event.target.textContent}`;
+
+    const data = await getData(url);
+
+    itemsList.innerHTML = "";
+
+    let items = data.results;
+
+    items.forEach(item => {
+        let itemHTML = createItemHTML(item);
+
+        itemsList.insertAdjacentHTML('beforeend', itemHTML); 
+    });
+
+    expandButtons = document.querySelectorAll('.expand-toggle');
+    hideButtons = document.querySelectorAll('.hide-toggle');
+
+    addButtonEvents();
 }
